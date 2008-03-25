16b5161c53556435fa92b7caaf049ac55c185eaf
Jay Phillips <jicksta+git@gmail.com>
2008-03-24 17:18:40 -0700

    from_trunk context now properly dials my desk phones

--- dialplan.rb
+++ dialplan.rb
@@ -12,7 +12,8 @@ desk {
 from_trunk {
   case extension
     when 415_524_4444, 650_305_2000, 409_291_4773, 44_20_3051_4843
-      dial "jay-desk-650", 'jay-desk-60'
+      dial "SIP/jay-desk-650", 'SIP/jay-desk-601', 'SIP/jay-desk-601-2',
+           :for => 1.minute, :caller_id => callerid
     else
       play 'invalid'
     end
