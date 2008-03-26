b4c6519964229cbc814c3d3adef5cae6017da803
Jay Phillips <jicksta+git@gmail.com>
2008-03-25 20:51:20 -0700

    Calls coming from the gate of our gated facility go to my roommate if I'm unavailable. Maybe he can let them in if necessary.

--- dialplan.rb
+++ dialplan.rb
@@ -14,8 +14,15 @@ from_trunk {
     when 415_524_4444, 650_305_2000, 409_291_4773, 44_20_3051_4843
       dial "SIP/jay-desk-650&SIP/jay-desk-601&SIP/jay-desk-601-2",
            :for => 15.seconds, :caller_id => callerid
-      ahn_log 'Trying cell phone.'
-      dial "SIP/14097672813@voipms", :caller_id => callerid if last_dial_unsuccessful?
-      dial "SIP/14097672813@nufone", :caller_id => callerid if last_dial_unsuccessful?
+      
+      alternatives = %w[SIP/14097672813@voipms SIP/14097672813@nufone]
+      
+      # Try roommate if it's someone at the gate trying to get in.
+      alternatives.insert(1, 'SIP/14154125674@voipms') if calleridname == 'EUREKA GARDENS'
+      
+      until last_dial_successful? || alternatives.empty?
+        ahn_log 'Trying cell phone.'
+        dial alternatives.shift, :caller_id => callerid, :for => 15.seconds
+      end
   end
 }
