01deb867a3aeda56c40a7b914ca27f0f4969ee5f
Jay Phillips <jicksta+git@gmail.com>
2008-03-25 20:58:28 -0700

    Cleaned up the dialplan a bit

--- dialplan.rb
+++ dialplan.rb
@@ -1,3 +1,9 @@
+Voipms = 'SIP/%s@voipms'
+Nufone = 'SIP/%s@nufone'
+Mobile = 1_409_767_2813
+Roomie = 1_415_412_5674
+MyDesk = 1_415_524_4444
+
 desk {
   extension_length = extension.to_s.length
   peer_extension = case extension_length
@@ -5,8 +11,8 @@ desk {
     when 10 : "1#{extension}"
     else extension
   end
-  dial "SIP/#{peer_extension}@voipms", :caller_id => "14155244444"
-  dial "SIP/#{peer_extension}@nufone", :caller_id => "14155244444" if last_dial_unsuccessful?
+  dial Voipms % peer_extension, :caller_id => MyDesk
+  dial Nufone % peer_extension, :caller_id => MyDesk if last_dial_unsuccessful?
 }
 
 from_trunk {
@@ -15,10 +21,10 @@ from_trunk {
       dial "SIP/jay-desk-650&SIP/jay-desk-601&SIP/jay-desk-601-2",
            :for => 15.seconds, :caller_id => callerid
       
-      alternatives = %w[SIP/14097672813@voipms SIP/14097672813@nufone]
+      alternatives = [Voipms % Mobile, Nufone % Mobile]
       
       # Try roommate if it's someone at the gate trying to get in.
-      alternatives.insert(1, 'SIP/14154125674@voipms') if calleridname == 'EUREKA GARDENS'
+      alternatives.insert(1, Voipms % Roomie) if calleridname == 'EUREKA GARDENS'
       
       until last_dial_successful? || alternatives.empty?
         ahn_log 'Trying cell phone.'
