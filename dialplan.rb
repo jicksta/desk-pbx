bf86bb692c1b23f3cc36d0f359010c371c42944b
Jay Phillips <jicksta+git@gmail.com>
2008-04-20 18:24:50 -0700

    defining TRANSFER_CONTEXT in the dialplan

--- dialplan.rb
+++ dialplan.rb
@@ -7,6 +7,7 @@ MyDesk = 1_415_524_4444
 desk {
   
   variable 'DYNAMIC_FEATURES' => 'atxfer'
+  variable 'TRANSFER_CONTEXT' => 'wtf'
   
   extension_length = extension.to_s.length
   peer_extension = case extension_length
@@ -27,7 +28,7 @@ invalid {
 from_trunk {
   
   variable 'DYNAMIC_FEATURES' => 'atxfer'
-  
+  variable 'TRANSFER_CONTEXT' => 'wtf'
   case extension
     when 415_524_4444, 650_305_2000, 409_291_4773, 44_20_3051_4843
       dial "SIP/jay-desk-650&SIP/jay-desk-601&SIP/jay-desk-601-2",
